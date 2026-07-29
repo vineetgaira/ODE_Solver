95f363c9ef51e8e90ea1d5fabccdc14afef3c5d9
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-29 08:03:10 +0530

    Starting the work today

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,14 @@
+import colorama 
+from colorama import Fore, Style
+colorama.init(autoreset=True)
+
 def get_user_input():
     valid_choices = {1, 2, 3, 4}
     try:
-        user = int(input("Enter your choice:"))
+        user = int(input(Fore.BLUE+"Enter your choice:"))
         if user in valid_choices:
             return user
         else:
-            print("Please enter a valid choice.")
+            print(Fore.RED+Style.BRIGHT+"Please enter a valid choice."+Style.RESET_ALL)
     except ValueError:
-        print("Please enter a valid choice.")
+        print(Fore.RED+Style.BRIGHT+"Please enter a valid choice."+Style.RESET_ALL)
