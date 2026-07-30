eed5622d03f439939461c20e79b2553120b328f3
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-30 08:41:00 +0530

    User input added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """This file will store the main project loop."""
 from src.menu import menu
-from src.input_handler import f
+from src.input_handler import user_ode
 
 def main():
     menu()
-    f()
+    user_ode()
+    
 
 if __name__ =="__main__":
     main()
--- src/input_handler.py
+++ src/input_handler.py
@@ -1,7 +1,17 @@
 """This file handles user input"""
 
 import sympy 
-from sympy import symbols, Eq
+from sympy import symbols, sympify, pprint
+
+def user_ode():
+
+    x, y = symbols('x y')
+
+    ode = input("Give an ODE in terms of x and y: ")
+
+    expression = sympify(ode)
+    pprint(expression, use_unicode = True)
+
 
 
 
