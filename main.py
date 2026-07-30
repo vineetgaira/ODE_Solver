9001b4b8d5d334e4c9ce2da7ab6f109ba0e0887c
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-30 09:44:19 +0530

    Menu done

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 """This file will store the main project loop."""
-from src.menu import show_banner, show_menu, method_menu
+import time
+from src.menu import show_banner, show_menu, method_menu, graph_menu
 
 
 def main():
     show_banner()
-    show_menu()
-    method_menu()
-    
-    
+
 
 if __name__ =="__main__":
     main()
--- src/menu.py
+++ src/menu.py
@@ -23,7 +23,7 @@ def show_menu():
 
 def method_menu():
     print(Fore.MAGENTA + "=" * 46)
-    print(Fore.GREEN + Style.BRIGHT + "             CHOOSE A METHOD")
+    print(Fore.GREEN + Style.BRIGHT + "           CHOOSE NUMERICAL METHOD")
     print(Fore.MAGENTA + "=" * 46)
 
     print(Fore.WHITE + "  [1] " + Fore.CYAN + "Euler Method")
@@ -32,4 +32,14 @@ def method_menu():
     print(Fore.WHITE + "  [4] " + Fore.CYAN + "Midpoint Method")
     print(Fore.WHITE + "  [5] " + Fore.CYAN + "RK2")
     print(Fore.WHITE + "  [6] " + Fore.CYAN + "RK4")
+
+def graph_menu():
+    print(Fore.MAGENTA + "=" * 26)
+    print(Fore.GREEN + Style.BRIGHT + "     Select Plot Type")
+    print(Fore.MAGENTA + "=" * 26)
+
+    print(Fore.WHITE + "  [1] " + Fore.CYAN + "Plot numerical solution")
+    print(Fore.WHITE + "  [2] " + Fore.CYAN + "Plot exact solution")
+    print(Fore.WHITE + "  [3] " + Fore.CYAN + "Plot both")
+
 
