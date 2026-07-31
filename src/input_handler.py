301ae7dee6e3eb095ffa9053be16a69abbf4980a
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-31 11:25:48 +0530

    Fix nested repo

--- src/input_handler.py
+++ src/input_handler.py
@@ -32,21 +32,25 @@ def get_valid_float(prompt, is_valid, error_message):
         except ValueError:
             print("Invalid value! Must be a number.")
 
-initial_x = get_valid_float("Initial x (x₀) : ", lambda v: v != 0, "Invalid value! Must not be 0.")
-initial_y = get_valid_float("Initial y (y₀) : ", lambda v: v != 0, "Invalid value! Must not be 0.")
-target_x = get_valid_float("Target x : ", lambda v: v != 0, "Invalid value! Must not be 0.")
-step_size = get_valid_float("Step size (h) : ", lambda v: v > 0, "Invalid value! Must be greater than 0.")
-exact_solution = get_valid_float("Enter the exact solution : ", lambda v: v != 0, "Invalid value! Must not be 0.")
-
-def all_initial_values(equation, initial_x, initial_y, target_x, step_size):
+def all_initial_values(equation, initial_x, initial_y, target_x, step_size, exact_solution):
 
     values = {
     "equation": equation,
     "x0": initial_x,
     "y0": initial_y,
     "target_x": target_x,
-    "step_size": step_size
+    "step_size": step_size,
+    "exact_solution": exact_solution
     }
 
     return values
+
+
+# initial_x = get_valid_float("Initial x (x₀) : ", lambda v: v != 0, "Invalid value! Must not be 0.")
+# initial_y = get_valid_float("Initial y (y₀) : ", lambda v: v != 0, "Invalid value! Must not be 0.")
+# target_x = get_valid_float("Target x : ", lambda v: v != 0, "Invalid value! Must not be 0.")
+# step_size = get_valid_float("Step size (h) : ", lambda v: v > 0, "Invalid value! Must be greater than 0.")
+# exact_solution = get_valid_float("Enter the exact solution : ", lambda v: v != 0, "Invalid value! Must not be 0.")
+
+
 
