e5f3d8bdeb38662df780247e632cdcd2f3c27878
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-02 10:28:19 +0530

    Input validation added

--- src/parser.py
+++ src/parser.py
@@ -8,4 +8,13 @@ from sympy import SympifyError
 ALLOWED_CHARS = set('xy0123456789+-*/.^ ')
 ALLOWED_WORDS = {'sin', 'cos', 'tan', 'exp', 'log','pi', 'sqrt', 'e' }
 
+x, y = symbols('x y')
+
+def is_valid_charset(eq: str) -> bool:
+
+    if not eq.strip():
+        return False
+    if len(eq) > 200:
+        return False
+    return set(eq.replace(' ', '')) <= (ALLOWED_CHARS | set(''.join(ALLOWED_WORDS)))
 
