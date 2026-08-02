3cd365e5188306bf271b6facea634fb50cd35953
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-02 10:29:10 +0530

    TRANSFORS added

--- src/parser.py
+++ src/parser.py
@@ -5,6 +5,10 @@ from sympy.parsing.sympy_parser import (standard_transformations, implicit_multi
 from sympy.parsing.sympy_parser import parse_expr
 from sympy import SympifyError
 
+TRANSFORMS = standard_transformations + (
+implicit_multiplication_application, convert_xor
+)
+
 ALLOWED_CHARS = set('xy0123456789+-*/.^ ')
 ALLOWED_WORDS = {'sin', 'cos', 'tan', 'exp', 'log','pi', 'sqrt', 'e' }
 
