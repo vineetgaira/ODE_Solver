c3d4ba5fc974e3cec8f486251cb6d76cd5d98b8a
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-04 16:25:47 +0530

    solver finish

--- src/solver.py
+++ src/solver.py
@@ -1,20 +1,22 @@
 
 """This one's job is which algorithm to run ."""
-from src.methods import euler, rk2, rk4
-METHODS= {"euler": euler,
-    "heun": rk2,
-    "midpoint": rk2,
-    "raltson": rk2,
+from src.methods import euler, heun, midpoint, ralston, rk4
+
+METHODS_REGISTRY= {"euler": euler,
+    "heun": heun,
+    "midpoint": midpoint,
+    "raltson": ralston,
     "rk4": rk4
 }
 
-def solve_with_method(method):
-
+def solve_with_method(method_name, equation, x0, y0, target_x, h):
+    method_fn = METHODS_REGISTRY[method_name]
+    history = [(x0, y0)]
+    n_steps = round((target_x - x0)/h)
+    for i in range(n_steps):
+        x0, y0 = method_fn(equation, x0, y0, h)
+        history.append((round(x0, 5), round(y0, 5)))
+    return history
     
-
-
-def solve():
-    pass
-
 def compare_methods():
     pass
