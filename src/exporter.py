69f2b4e86b0c2ea7c7948c9c14abbf9314d402d2
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-06 09:10:39 +0530

    Fix the header in exporter

--- src/exporter.py
+++ src/exporter.py
@@ -1,11 +1,13 @@
 """This file creates csv files"""
+import os
 import csv
 
 def export_problem_csv(current_problem):
+    file_exists = os.path.exists("data/problems.csv") and os.path.getsize("data/problems.csv") > 0
     with open("data/problems.csv", "a", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=current_problem.keys())
-
-        writer.writeheader()      # Writes the keys
+        if not file_exists:
+            writer.writeheader()      # Writes the keys
         writer.writerow(current_problem)
 
 def export_txt():
