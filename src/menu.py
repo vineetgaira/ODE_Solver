c5b7576fdaa442980f62737f9d6dfad47242f379
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-09 09:28:18 +0530

    Feature: Settings module created

--- src/menu.py
+++ src/menu.py
@@ -51,9 +51,7 @@ def show_settings_menu():
     print(Fore.WHITE + "  [2] " + Fore.LIGHTCYAN_EX + "Output colour")
     print(Fore.WHITE + "  [3] " + Fore.LIGHTCYAN_EX + "Save results automatically")
     print(Fore.WHITE + "  [4] " + Fore.LIGHTCYAN_EX + "Graph style")
-    print(Fore.WHITE + "  [5] " + Fore.RED + "Return")
     
-
 def show_help_menu():
     print(Fore.MAGENTA + "=" * 26)
     print(Fore.GREEN + Style.BRIGHT + "    Supported Functions")
---
+++ src/settings.py
@@ -0,0 +1,33 @@
+import json
+import os 
+
+SETTINGS_FILE = "data/settings.json"
+
+DEFAULT_SETTINGS = {
+    "precison": 5,
+    "colour": True,
+    "save_automatically": True,
+    "graph_style": "line"
+}
+
+_settings = DEFAULT_SETTINGS.copy()
+
+def load_settings():
+    global _settings
+    if os.path.exists(SETTINGS_FILE) and os.path.getsize(SETTINGS_FILE) > 0:
+        with open ("SETTINGS_FILE", "r") as file:
+            saved = json.load(file)
+        _settings = {**DEFAULT_SETTINGS, **saved}
+    else:
+        _settings = DEFAULT_SETTINGS.copy()
+
+def save_settings():
+    with open("SETTINGS_FILE", "w") as file:
+        json.dump(_settings, file, sort_keys=True, indent=2)
+
+def get_setting_key(key):
+    return _settings[key]
+
+def set_settings(key, value):
+    _settings[key] = value
+    save_settings()
